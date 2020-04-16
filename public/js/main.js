6a954a9d33891ace1619c7cf5587cdb156cd9cf2
buna_branislav <branislav.buna@spiressl.com>
2020-04-16 14:29:42 +0100

    users + room updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,18 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 
+// Get username and room from URL
+const { username,room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+
 const socket = io();
+
+// Join chat room
+socket.emit('joinRoom', { username, room });
 
 // Message from server
 socket.on('message',message => {
-    console.log(message);
     outputMessage(message);
 
     // Scroll down
@@ -32,9 +39,9 @@ chatForm.addEventListener('submit', (e) => {
 function outputMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-        ${message}
+        ${message.text}
     </p>`;
     document.querySelector('.chat-messages').appendChild(div);
 }
